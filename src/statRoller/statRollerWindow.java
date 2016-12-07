37d899130545b8a7cd14c19e1795b8e9d74c612f
Blake Bennett <you@example.com>
2016-12-07 16:08:44 -0600

    added some comments

--- src/statRoller/statRollerWindow.java
+++ src/statRoller/statRollerWindow.java
@@ -1,6 +1,8 @@
 package statRoller;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Blake Bennett on 12/7/2016.
@@ -8,6 +10,26 @@ import javax.swing.*;
 public class statRollerWindow {
     public JPanel main;
     private JLabel Strength;
-    private JTextField textField1;
-    private JButton randomButton;
+    private JTextField strengthTextFeild;
+    private JButton strButton;
+    private JTextField conTextFeild;
+    private JButton conButton;
+    private JTextField dexTextFeild;
+    private JButton dexButton;
+    private JFormattedTextField intTextFeild;
+    private JButton intButton;
+    private JTextField wisdomTextFeild;
+    private JButton widButton;
+    private JButton chaButton;
+    private JTextField chaTextFeild;
+    private JButton roleAllButton;
+
+    public statRollerWindow() {
+        strButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Strength button pressed");
+            }
+        });
+    }
 }
